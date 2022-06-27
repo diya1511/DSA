fe76c9bd2fc9a1eccbe4b9f73b32507475caa55f
diya1511 <diyaagrawal1511@gmail.com>
2022-06-28 00:55:19 +0530

    Dynamic Allocation and  Recursion

---
+++ Recursion/reversestring.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void solve(string&str,int s , int e ){
+    if(s>e){
+        return ;
+    }
+    swap(str[s],str[e]);
+    s++;
+    e--;
+  solve(str,s,e);
+}
+
+string reverseString(string str)
+{   int e =str.length()-1;
+	solve(str,0,e);
+    return str;
+}
