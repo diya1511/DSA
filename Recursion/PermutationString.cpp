188dbc1ccc80119cd72cdcea35939d3d15c09381
diya1511 <diyaagrawal1511@gmail.com>
2022-06-30 19:09:02 +0530

    Permutation Done

---
+++ Recursion/PermutationString.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+#include <bits/stdc++.h>
+class Solution {
+void solve(vector<int>& nums,int index, vector<vector<int>>&ans){   
+        //base case 
+     while(index >= nums.size()){
+             ans.push_back(nums);
+             return ;
+     }
+        for(int i = index ; i < nums.size(); i++){
+                swap(nums[index],nums[i]);
+                solve(nums,index+1,ans);
+                //backtracking
+                swap(nums[index],nums[i]);
+        }
+}
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans ; 
+         int index = 0 ; 
+          solve(nums,index,ans);
+           return ans;
+    }
+};
