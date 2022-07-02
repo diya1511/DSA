5b4994bf2545606e976a754303f6ca688e897fad
diya1511 <diyaagrawal1511@gmail.com>
2022-07-02 21:05:44 +0530

    Stack Problem Added

---
+++ 1-DArray/moveZeroes.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std ;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int nonZero=0 ;
+            for(int i = 0 ; i<=nums.size()-1;i++){
+            if(nums[i]!=0){
+            swap(nums[i],nums[nonZero]);
+            nonZero++;
+            }        
+            }
+    }
+};
---
+++ Stack/reverseString.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    stack <char> s;
+    for(int i = 0 ; i < str.length() ; i++){
+     char ch =str[i];
+     s.push(ch);
+    }
+    while(!s.empty()){
+        cout<<s.top();
+        s.pop();
+
+    }
+    return 0;
+}
